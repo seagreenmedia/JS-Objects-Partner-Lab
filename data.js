122e41409fb607c61bffa3ad88061bd4d5681b46
John Redden <johnleightonredden@gmail.com>
2020-12-12 11:45:40 -0500

    started part 2

--- data.js
+++ data.js
@@ -13,51 +13,58 @@
 // console.log(mySpecialObject.key);
 // console.log(mySpecialObject.hello());
 
-let user = {
-    someParameter: ''
-};
+// let user = {
+//     someParameter: ''
+// };
 
-function gatherData(event) {
-    event.preventDefault();
-    let myForm = document.getElementById("myForm");
-    let formData = new FormData(myForm);
-    console.log(myForm);
-    const firstName = formData.get('user_firstName');
-    const lastName = formData.get('user_lastName');
-    const age = formData.get('age');
-    const password = formData.get('password');
-    const passwordConf = formData.get('passwordConf');
-    console.log(firstName, lastName, age, password, passwordConf);
+// function gatherData(event) {
+//     event.preventDefault();
+//     let myForm = document.getElementById("myForm");
+//     let formData = new FormData(myForm);
+//     console.log(myForm);
+//     const firstName = formData.get('user_firstName');
+//     const lastName = formData.get('user_lastName');
+//     const age = formData.get('age');
+//     const password = formData.get('password');
+//     const passwordConf = formData.get('passwordConf');
+//     console.log(firstName, lastName, age, password, passwordConf);
 
-    user = {
-        firstName: firstName,
-        lastName: lastName,
-        age: age,
-        password: password,
-        passwordConf: passwordConf
-    }
+//     user = {
+//         firstName: firstName,
+//         lastName: lastName,
+//         age: age,
+//         password: password,
+//         passwordConf: passwordConf
+//     }
 
-    console.log(user);
+//     console.log(user);
+// }
+
+// document.getElementById("submitButton").addEventListener("click", gatherData);
+
+// let object = {}
+
+// let person = {
+//     firstName: 'Joh',
+//     lastName: 'Doe',
+//     age: '26',
+// };
+
+// console.log(person['firstName']); // John
+// console.log(person.firstName); // John
+// console.log(person.lastName) // silly
+// console.log(person.age); //cheese
+
+// // some other type of task
+// // ....
+
+// person.job = 'Software Developer';
+
+// console.log(person.job) // Software Developer
+
+let license = {
+    name: 'John',
+    DOB: '12/9/89',
+    maritalStatus: false
 }
-
-document.getElementById("submitButton").addEventListener("click", gatherData);
-
-let object = {}
-
-let person = {
-    firstName: 'Joh',
-    lastName: 'Doe',
-    age: '26',
-};
-
-console.log(person['firstName']); // John
-console.log(person.firstName); // John
-console.log(person.lastName) // silly
-console.log(person.age); //cheese
-
-// some other type of task
-// ....
-
-person.job = 'Software Developer';
-
-console.log(person.job) // Software Developer
+console.log(license);
